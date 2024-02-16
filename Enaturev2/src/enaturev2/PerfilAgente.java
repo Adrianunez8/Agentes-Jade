c50f119b5502530b4fe91f8e9eb873e04d40a6a9
Adrianunez8 <adrian.nunez@unl.edu.ec>
2024-02-16 11:53:23 -0500

    Algortimo KNN para la recomendaciones

--- Enaturev2/src/enaturev2/PerfilAgente.java
+++ Enaturev2/src/enaturev2/PerfilAgente.java
@@ -1,133 +1,141 @@
 package enaturev2;
 
 //import jade.core.Agent;
-//import jade.core.behaviours.CyclicBehaviour;
-//import jade.lang.acl.ACLMessage;
+//import java.util.ArrayList;
+//import java.util.List;
+//import jade.core.behaviours.OneShotBehaviour;
+//import jade.core.behaviours.SequentialBehaviour;
 //
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.util.HashMap;
+//import java.util.Map;
 //import java.util.Scanner;
 //
 //public class PerfilAgente extends Agent {
-//    private String estiloAprendizaje;
-//    private String inteligenciaMultiple;
-//    private String nombre;
-//    private String edad;
+//
+//    private Map<String, String[]> perfilesEstudiantes;
 //
 //    protected void setup() {
-//        addBehaviour(new IngresarDatosBehavior());
+//        perfilesEstudiantes = leerDatosDesdeArchivo(System.getProperty("user.home") + File.separator + "Escritorio" + File.separator + "datos_estudiantes.txt");
+//
+//        SequentialBehaviour secuencia = new SequentialBehaviour();
+//        secuencia.addSubBehaviour(new EnviarPerfilBehavior());
+//        addBehaviour(secuencia);
 //    }
 //
-//    private class IngresarDatosBehavior extends CyclicBehaviour {
+//    private class EnviarPerfilBehavior extends OneShotBehaviour {
 //        public void action() {
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.println("*****Bienvenido al AGENTE SMA Enature****** ");
-//            System.out.println(getLocalName() + ": Ingrese el nombre: ");
-//            nombre = scanner.nextLine();
+//            // Enviar información de perfil al agente recomendador
+//            for (Map.Entry<String, String[]> entry : perfilesEstudiantes.entrySet()) {
+//                String estudiante = entry.getKey();
+//                String[] perfil = entry.getValue();
 //
-//            System.out.println(getLocalName() + ": Ingrese la edad: ");
-//            edad = scanner.nextLine();
+//                System.out.println("PerfilEstudiante: Enviando perfil a AgenteRecomendador - Estudiante: " + estudiante +
+//                        ", EstiloAprendizaje: " + perfil[0] + ", InteligenciasMultiples: " + perfil[1]);
 //
-//            System.out.println(getLocalName() + ": Ingresa el estilo de aprendizaje (visual, auditivo, kinestesico): ");
-//            estiloAprendizaje = scanner.nextLine();
+//                // Crear mensaje
+//                String contenidoMensaje = perfil[0] + "," + perfil[1];
+//                enviarMensajeAgenteRecomendador(estudiante, contenidoMensaje);
 //
-//            System.out.println(getLocalName() + ": Ingresa la inteligencia múltiple (logico-matematica, linguistica, interpersonal): ");
-//            inteligenciaMultiple = scanner.nextLine();
-//
-//            // Esperamos un momento para asegurarnos de que el agente Recomendador esté registrado
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
+//                // Esperar un poco antes de enviar otro perfil (puedes ajustar esto según tus necesidades)
+//                block(5000);  // Esperar 5 segundos
 //            }
-//
-//            enviarDatosAlRecomendador();
-//
-//            // Terminamos este comportamiento
-//            myAgent.removeBehaviour(this);
 //        }
 //
-//        private void enviarDatosAlRecomendador() {
-//            ACLMessage mensaje = new ACLMessage(ACLMessage.REQUEST);
-//            mensaje.setContent(estiloAprendizaje + "," + inteligenciaMultiple);
-//            mensaje.addReceiver(getAID("RecomendadorAgente"));
+//        private void enviarMensajeAgenteRecomendador(String estudiante, String contenidoMensaje) {
+//            jade.lang.acl.ACLMessage mensaje = new jade.lang.acl.ACLMessage(jade.lang.acl.ACLMessage.INFORM);
+//            mensaje.addReceiver(new jade.core.AID("RecomendadorAgente", jade.core.AID.ISLOCALNAME));
+//            mensaje.setContent(contenidoMensaje);
 //            send(mensaje);
 //        }
 //    }
+//
+//    private Map<String, String[]> leerDatosDesdeArchivo(String rutaCompleta) {
+//        Map<String, String[]> perfiles = new HashMap<>();
+//
+//        try {
+//            File archivo = new File(rutaCompleta);
+//            Scanner scanner = new Scanner(archivo);
+//
+//            while (scanner.hasNextLine()) {
+//                String linea = scanner.nextLine();
+//                String[] partes = linea.split(",");
+//                if (partes.length == 3) {
+//                    String estudiante = partes[0];
+//                    String estiloAprendizaje = partes[1];
+//                    String inteligenciasMultiples = partes[2];
+//
+//                    // Conectar los datos a las variables de inteligencias múltiples
+//                    perfiles.put(estudiante, new String[]{estiloAprendizaje, inteligenciasMultiples});
+//                }
+//            }
+//
+//            scanner.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return perfiles;
+//    }
 //}
+
+
+
+
+///// vale con KNN
 
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
-import jade.core.behaviours.SequentialBehaviour;
-
+import jade.lang.acl.ACLMessage;
+import jade.core.AID;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class PerfilAgente extends Agent {
 
-    private Map<String, String[]> perfilesEstudiantes;
-
     protected void setup() {
-        perfilesEstudiantes = leerDatosDesdeArchivo(System.getProperty("user.home") + File.separator + "Escritorio" + File.separator + "datos_estudiantes.txt");
-
-        SequentialBehaviour secuencia = new SequentialBehaviour();
-        secuencia.addSubBehaviour(new EnviarPerfilBehavior());
-        addBehaviour(secuencia);
+        addBehaviour(new EnviarDatosPerfilBehaviour());
     }
 
-    private class EnviarPerfilBehavior extends OneShotBehaviour {
+    private class EnviarDatosPerfilBehaviour extends OneShotBehaviour {
         public void action() {
-            // Enviar información de perfil al agente recomendador
-            for (Map.Entry<String, String[]> entry : perfilesEstudiantes.entrySet()) {
-                String estudiante = entry.getKey();
-                String[] perfil = entry.getValue();
-
-                System.out.println("PerfilEstudiante: Enviando perfil a AgenteRecomendador - Estudiante: " + estudiante +
-                        ", EstiloAprendizaje: " + perfil[0] + ", InteligenciasMultiples: " + perfil[1]);
-
+            ArrayList<String> datosEstudiantes = leerDatosEstudiantesDesdeArchivo(System.getProperty("user.home") + File.separator + "Escritorio" + File.separator + "perfil_estudiante.arff");
+            if (!datosEstudiantes.isEmpty()) {
                 // Crear mensaje
-                String contenidoMensaje = perfil[0] + "," + perfil[1];
-                enviarMensajeAgenteRecomendador(estudiante, contenidoMensaje);
+                ACLMessage mensaje = new ACLMessage(ACLMessage.INFORM);
+                mensaje.setContent(String.join(",", datosEstudiantes));
 
-                // Esperar un poco antes de enviar otro perfil (puedes ajustar esto según tus necesidades)
-                block(5000);  // Esperar 5 segundos
+                // Definir destinatario
+                AID idAgenteRecomendador = new AID("RecomendadorAgente", AID.ISLOCALNAME);
+                mensaje.addReceiver(idAgenteRecomendador);
+
+                // Enviar mensaje
+                send(mensaje);
+            } else {
+                System.out.println("No se encontraron datos de estudiantes en el archivo.");
             }
         }
 
-        private void enviarMensajeAgenteRecomendador(String estudiante, String contenidoMensaje) {
-            jade.lang.acl.ACLMessage mensaje = new jade.lang.acl.ACLMessage(jade.lang.acl.ACLMessage.INFORM);
-            mensaje.addReceiver(new jade.core.AID("RecomendadorAgente", jade.core.AID.ISLOCALNAME));
-            mensaje.setContent(contenidoMensaje);
-            send(mensaje);
-        }
-    }
-
-    private Map<String, String[]> leerDatosDesdeArchivo(String rutaCompleta) {
-        Map<String, String[]> perfiles = new HashMap<>();
-
-        try {
-            File archivo = new File(rutaCompleta);
-            Scanner scanner = new Scanner(archivo);
-
-            while (scanner.hasNextLine()) {
-                String linea = scanner.nextLine();
-                String[] partes = linea.split(",");
-                if (partes.length == 3) {
-                    String estudiante = partes[0];
-                    String estiloAprendizaje = partes[1];
-                    String inteligenciasMultiples = partes[2];
-
-                    // Conectar los datos a las variables de inteligencias múltiples
-                    perfiles.put(estudiante, new String[]{estiloAprendizaje, inteligenciasMultiples});
+        private ArrayList<String> leerDatosEstudiantesDesdeArchivo(String rutaArchivo) {
+            ArrayList<String> datosEstudiantes = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+                String linea;
+                while ((linea = br.readLine()) != null) {
+                    datosEstudiantes.add(linea);
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            return datosEstudiantes;
         }
-
-        return perfiles;
     }
 }
+
+
+
+
+
--- Enaturev2/src/enaturev2/RecomendadorAgente.java
+++ Enaturev2/src/enaturev2/RecomendadorAgente.java
@@ -1,167 +1,91 @@
 package enaturev2;
 
-//
-//import jade.core.Agent;
-//import jade.core.behaviours.CyclicBehaviour;
-//import jade.lang.acl.ACLMessage;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class RecomendadorAgente extends Agent {
-//
-//    private Map<String, String[]> perfilesEstudiantes;
-//
-//    protected void setup() {
-//        perfilesEstudiantes = new HashMap<>();
-//
-//        addBehaviour(new RecibirPerfilBehavior());
-//    }
-//
-//    private class RecibirPerfilBehavior extends CyclicBehaviour {
-//        public void action() {
-//            ACLMessage mensaje = receive();
-//            if (mensaje != null) {
-//                // Obtener datos del mensaje
-//                String[] datos = mensaje.getContent().split(",");
-//                String estudiante = mensaje.getSender().getLocalName();
-//                String estiloAprendizaje = datos[0];
-//                String inteligenciasMultiples = datos[1];
-//                //System.arraycopy(datos, 1, inteligenciasMultiples, 0, inteligenciasMultiples.length);
-//
-//                // Almacenar el perfil del estudiante
-//                perfilesEstudiantes.put(estudiante, new String[]{estiloAprendizaje, String.join(",", inteligenciasMultiples)});
-//
-//                // Realizar recomendación basada en el estilo de aprendizaje e inteligencias múltiples
-//                String recomendacion = recomendarActividad(estiloAprendizaje, inteligenciasMultiples);
-//
-//                // Enviar la recomendación al estudiante
-//                enviarRecomendacionAlEstudiante(estudiante, recomendacion);
-//            } else {
-//                block();
-//            }
-//        }
-//
-//        private String recomendarActividad(String estiloAprendizaje, String inteligenciasMultiples) {
-//            // Lógica de recomendación basada en el estilo de aprendizaje e inteligencias múltiples
-//            // (Este es un algoritmo ficticio; debes reemplazarlo con tu lógica real)
-//               String actividadRecomendada = "";
-////
-//            // Comprobamos si el estilo de aprendizaje es visual y la inteligencia múltiple es interpersonal
-//            if (estiloAprendizaje.equalsIgnoreCase("Visual")
-//                    && inteligenciasMultiples.equalsIgnoreCase("Interpersonal")) {
-//                actividadRecomendada = "Ver Videos en YouTube del tema de interes";
-//            } 
-//            else if(estiloAprendizaje.equalsIgnoreCase("Auditivo")
-//                    && inteligenciasMultiples.equalsIgnoreCase("Naturalista")) {
-//                actividadRecomendada = "Escuchar potscast educativos y musica clasica para relajacion";
-//            }
-//            else {
-//                actividadRecomendada = "Refuerzos con videos del tema a tratar";
-//            }
-//
-//            return actividadRecomendada;
-//         
-//        }
-//
-//        private void enviarRecomendacionAlEstudiante(String estudiante, String recomendacion) {
-//            // Enviar las recomendaciones al estudiante
-//            ACLMessage mensaje = new ACLMessage(ACLMessage.INFORM);
-//            mensaje.addReceiver(getAID(estudiante));
-//            mensaje.setContent(recomendacion);
-//            send(mensaje);
-//
-//            System.out.println("RecomendadorAgente: Enviando recomendación a perfilAgente - Te recomiendo: " + recomendacion);
-//        }
-//    }
-//}
-
 import jade.core.Agent;
-import jade.core.behaviours.CyclicBehaviour;
-import jade.lang.acl.ACLMessage;
+import weka.core.*;
+import weka.core.neighboursearch.*;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class RecomendadorAgente extends Agent {
 
-    private List<String> actividadesRecomendadas;
+    private Instances data;
 
     protected void setup() {
-        actividadesRecomendadas = leerActividadesDesdeArchivo(System.getProperty("user.home") + File.separator + "Escritorio" + File.separator + "actividades_recomendadas.txt");
+        // Cargar los datos de actividades desde un archivo ARFF
+        cargarDatosActividades(System.getProperty("user.home") + File.separator + "Escritorio" + File.separator + "actividades.arff");
 
-        addBehaviour(new RecibirPerfilBehavior());
-    }
+        // Generar recomendaciones
+        String estilo = "visual";
+        String inteligencia = "linguistica";
+        List<Recomendacion> recomendaciones = generarRecomendaciones(estilo, inteligencia);
 
-    private class RecibirPerfilBehavior extends CyclicBehaviour {
-        public void action() {
-            ACLMessage mensaje = receive();
-            if (mensaje != null) {
-                // Obtener datos del mensaje
-                String[] datos = mensaje.getContent().split(",");
-                String estudiante = mensaje.getSender().getLocalName();
-                String estiloAprendizaje = datos[0];
-                String inteligenciaMultiple = datos[1];
-
-                // Realizar recomendación basada en el estilo de aprendizaje e inteligencia múltiple
-                String recomendacion = recomendarActividad(estiloAprendizaje, inteligenciaMultiple);
-
-                // Enviar la recomendación al estudiante
-                enviarRecomendacionAlEstudiante(estudiante, recomendacion);
-            } else {
-                block();
-            }
-        }
-
-        private String recomendarActividad(String estiloAprendizaje, String inteligenciaMultiple) {
-            for (String actividad : actividadesRecomendadas) {
-                String[] partes = actividad.split(":");
-                if (partes.length == 2) {
-                    String[] combinacion = partes[0].split(",");
-                    String recomendacion = partes[1].trim();
-
-                    // Verificar si la combinación coincide con el estilo de aprendizaje e inteligencia múltiple
-                    if (combinacion.length == 2 && combinacion[0].equalsIgnoreCase(estiloAprendizaje)
-                            && combinacion[1].equalsIgnoreCase(inteligenciaMultiple)) {
-                        return recomendacion;
-                    }
-                }
-            }
-            return "No tengo una recomendación específica en este momento.";
-        }
-
-        private void enviarRecomendacionAlEstudiante(String estudiante, String recomendacion) {
-            // Enviar las recomendaciones al estudiante
-            ACLMessage mensaje = new ACLMessage(ACLMessage.INFORM);
-            mensaje.addReceiver(getAID(estudiante));
-            mensaje.setContent(recomendacion);
-            send(mensaje);
-
-            System.out.println("RecomendadorAgente: Enviando recomendación a " + estudiante + " - Te recomiendo: " + recomendacion);
+        // Imprimir las recomendaciones
+        for (Recomendacion recomendacion : recomendaciones) {
+            System.out.println("Estudiante: " + recomendacion.getEstudiante() + ", Actividad: " + recomendacion.getActividad());
         }
     }
 
-    private List<String> leerActividadesDesdeArchivo(String rutaCompleta) {
-        List<String> actividades = new ArrayList<>();
-
+    private void cargarDatosActividades(String rutaArchivo) {
         try {
-            File archivo = new File(rutaCompleta);
-            Scanner scanner = new Scanner(archivo);
+            BufferedReader reader = new BufferedReader(new FileReader(rutaArchivo));
+            data = new Instances(reader);
+            reader.close();
+            data.setClassIndex(data.numAttributes() - 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-            while (scanner.hasNextLine()) {
-                String actividad = scanner.nextLine();
-                actividades.add(actividad);
+    private List<Recomendacion> generarRecomendaciones(String estilo, String inteligencia) {
+        List<Recomendacion> recomendaciones = new ArrayList<>();
+
+        // Crear una nueva instancia con los atributos estilo e inteligencia
+        Instance instancia = new DenseInstance(3);
+        Attribute estiloAttr = data.attribute("estilo_aprendizaje");
+        Attribute inteligenciaAttr = data.attribute("inteligencia_multiple");
+        instancia.setValue(estiloAttr, estilo);
+        instancia.setValue(inteligenciaAttr, inteligencia);
+
+        // Calcular la distancia entre la instancia y todas las demás instancias
+        NearestNeighbourSearch knn = new LinearNNSearch(data);
+        try {
+            Instances vecinos = knn.kNearestNeighbours(instancia, 3); // Obtener los 3 vecinos más cercanos
+            // Recuperar las actividades y el estudiante de los vecinos más cercanos como recomendaciones
+            for (int i = 0; i < vecinos.numInstances(); i++) {
+                String estudiante = vecinos.instance(i).stringValue(vecinos.attribute("estudiante"));
+                String actividad = vecinos.instance(i).stringValue(data.classAttribute());
+                recomendaciones.add(new Recomendacion(estudiante, actividad));
             }
-
-            scanner.close();
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return actividades;
+        return recomendaciones;
+    }
+
+    // Clase para representar una recomendación con el estudiante correspondiente
+    private static class Recomendacion {
+        private String estudiante;
+        private String actividad;
+
+        public Recomendacion(String estudiante, String actividad) {
+            this.estudiante = estudiante;
+            this.actividad = actividad;
+        }
+
+        public String getEstudiante() {
+            return estudiante;
+        }
+
+        public String getActividad() {
+            return actividad;
+        }
     }
 }
+
 
